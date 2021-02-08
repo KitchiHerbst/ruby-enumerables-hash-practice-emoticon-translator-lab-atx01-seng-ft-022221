1e6f23674d60ac6484a956064496bce29ba05245
Triston Herbst <tristonherbst97@gmail.com>
2021-02-08 21:12:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library(path)
+  load_library(path).each do |key, value|
+    binding.pry
 end
 
 def get_english_meaning(path, emoticon)
